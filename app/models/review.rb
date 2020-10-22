1a48e05f3957f57d6ccdad89f5fbeabb96c8e46c
hayashihiroaki <61njn0@gmail.com>
2020-10-22 16:23:42 +0900

    いいね機能

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :shop
+  has_many :favorites, dependent: :destroy
+  
   validates :score, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users
   resources :shops do
-    resources :reviews, only: [:index, :create]
+    resources :reviews, only: [:index, :create] do
+      resource :favorites, only: [:create, :destroy]
+    end
   end
 end
